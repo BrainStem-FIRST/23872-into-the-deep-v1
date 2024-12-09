c17fc95edb627dbb680b0409f4c8947c876dd957
Dave Whitmer <you@example.com>
2024-12-09 08:11:31 -0500

    Update meep meep paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,14 +12,33 @@ public class MeepMeepTesting {
         MeepMeep meepMeep = new MeepMeep(800);
 
         Pose2d beginPose = new Pose2d(-39, -64, Math.toRadians(0));
-        Pose2d depositPreloadPose = new Pose2d(-57, -63.5, Math.toRadians(30));
+        Pose2d depositPose = new Pose2d(-59, -59, Math.toRadians(50));
+        Pose2d rightBlockPose = new Pose2d(-47, -49, Math.toRadians(90));
+        Pose2d centerBlockPose = new Pose2d(-59, -45, Math.toRadians(92));
+        Pose2d leftBlockPose = new Pose2d(-52, -32, Math.toRadians(160));
+        Pose2d parkPose = new Pose2d(-20, -12, Math.toRadians(0));
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13.3)
+                .setDimensions(15,15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(beginPose)
                         .setReversed(true)
-                        .splineToLinearHeading(depositPreloadPose, Math.toRadians(210))
+                        .splineToLinearHeading(depositPose, Math.toRadians(210))
+                        .setReversed(false)
+                        .splineToLinearHeading(rightBlockPose, Math.toRadians(75))
+                        .setReversed(true)
+                        .splineToLinearHeading(depositPose, Math.toRadians(210))
+                        .setReversed(false)
+                        .splineToLinearHeading(centerBlockPose, Math.toRadians(90))
+                        .setReversed(true)
+                        .splineToLinearHeading(depositPose, Math.toRadians(225))
+                        .setReversed(false)
+                        .splineToLinearHeading(leftBlockPose, Math.toRadians(110))
+                        .setReversed(true)
+                        .splineToLinearHeading(depositPose, Math.toRadians(225))
+                        .setReversed(false)
+                        .splineToLinearHeading(parkPose, Math.toRadians(0))
                         .build());
 
 
