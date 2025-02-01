846217e4e68ac88b9b1decc17a34c7f92694fd09
mfarmar <69918859+mfarmar@users.noreply.github.com>
2025-02-01 09:39:08 -0500

    auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/YellowBlocks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/YellowBlocks.java
@@ -20,7 +20,7 @@ public class YellowBlocks extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         Pose2d beginPose = new Pose2d(-39, -64, Math.toRadians(0));
-        Pose2d depositPose = new Pose2d(-59, -59, Math.toRadians(65));
+        Pose2d depositPose = new Pose2d(-59, -59.5, Math.toRadians(65));
         Pose2d rightBlockPose = new Pose2d(-47, -44, Math.toRadians(90));
         Pose2d centerBlockPose = new Pose2d(-56.5, -43.5, Math.toRadians(89));
         Pose2d leftBlockPose = new Pose2d(-52, -32, Math.toRadians(160));
